23fb120d9a510dffb0843cf1129669ec82332e43
sravanthiin <51396060+sravanthiin@users.noreply.github.com>
2019-06-12 12:07:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,27 @@
 /*
- * DCMOTOR BUTTON.c
+ * DCMOTOR1.c
  *
- * Created: 11-06-2019 11:06:59
+ * Created: 11-06-2019 10:52:42
  * Author : Mohit
  */ 
 
 #include <avr/io.h>
 #define F_CPU 16000000UL
-#include <util/delay.h>
-#include <stdlib.h>
+#include<util/delay.h>
+#include<stdlib.h>
 
 
 int main(void)
 {
-   DDRD=0X00;
-    while (1) 
-    {
-		if((PINB&(1<<PB0))==1)
-		{
-	    	PORTD|=(1<<PD0);
-			_delay_ms(500);
-			PORTD&=(1<<PD1);
-			_delay_ms(500);
-		}
-		if((PINB&(1<<PB1))==0)
-		 {
-			 PORTD|=(1<<PD0);
-			 _delay_ms(500);
-			 PORTD&=~(1<<PD0);
-			 _delay_ms(500);
-		 }
+    DDRD=0X00;
+	while(1)
+	{
+		PORTD=0X01;
+		_delay_ms(800);
+		PORTD=0X02;
+		PORTD=0X00;
+		_delay_ms(800);
 	}
+	
 }
+
