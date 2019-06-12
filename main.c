c7a0d69cd070b73410145822647dc6f4bc2faace
sravanthiin <51396060+sravanthiin@users.noreply.github.com>
2019-06-12 14:36:35 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,29 @@
 /*
- * buzzer.c
+ * timer.c
  *
- * Created: 11-06-2019 22:37:36
- * Author : Mohit
+ * Created: 10-06-2019 12:13:04
+ * Author : mohit
  */ 
 
-
 #include <avr/io.h>
-#define F_CPU 1000000UL
-#include<util/delay.h>
-#include<stdlib.h>
-#define E5
-#define RS 7
-void send_a_command(unsigned char command);
-void send_a_character(unsigned char character);
-void send_a_string(char*string_of_character);
-
-int main(void)
+#define LED PB0
+int main()
 {
-	DDRC=0XFF;
-	DDRA=0X00;
-	DDRD=0XFF;
-	_delay_ms(50);
-	ADMUX|=(1<<REFS0)|(1<<REFS1);
-	ADCSRA|=(1<<ADEN)|(1<<ADATE)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
-	int COUNTA=0;
-	char SHOWA[3];
-	send_a_command(0x01);
-	_delay_ms(50);
-	send_a_command(0x38);
-	
-	while (1)
+	int timeroverflowcount =0;
+	DDRB =0XFF;
+	TCNT0=0X00;
+	TCCR0=(1<<CS00)|(1<<CS02);
+	while(1)
 	{
+		while((TIFR&0X01)==0);
+		TCNT0=0X00;
+		TIFR=0X01;
+		timeroverflowcount++;
+		if(timeroverflowcount >=5)
+		{
+			PORTB^=(0X11<<LED);
+			timeroverflowcount = 0;
+		}
 	}
+	
 }
-
